4a7a117df084fef2ba966b2675b0f6941c68d0f8
MikeyRG <MikeyRG@DESKTOP-G1MTSAT>
2023-10-29 09:57:48 -0400

    1.0.3

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,26 @@
+"use client"
 import Image from "next/image";
 import Link from 'next/link'
+import { useEffect } from "react";
+import { usePathname } from 'next/navigation'
 const Navbar = () => {
+
+  const pathname = usePathname()
+  useEffect(() => {
+    const navbarOptions = document.querySelectorAll(".nav-sections");
+    navbarOptions.forEach(element => {
+      element.addEventListener('click', () => {
+        navbarOptions.forEach(element => {
+          element.classList.remove('selected');
+        })
+        element.classList.toggle('selected')
+      })
+    });
+
+  }, []);
+
   return (
     <nav className="navbar">
-
       <Image
         src="/assets/icons/mikey.png"
         alt="Picture of the author"
@@ -14,11 +31,11 @@ const Navbar = () => {
 
       <div className="navbar-menu">
         <Link className="nav-sections selected" href="/">HOME</Link>
-        <Link className="nav-sections " href="/about">ABOUT</Link>
-        <Link className="nav-sections " href="/work">WORK</Link>
+        <Link className="nav-sections" href="/about">ABOUT</Link>
+        <Link className="nav-sections" href="/work">WORK</Link>
         <div className="language-selection">
           <span className="language-spanish ">ESP</span>
-          <span className="language-english selected">EN</span>
+          <span className="language-english ">EN</span>
         </div>
         <Link className="button-get-in-touch" href="/getintouch">GET IN TOUCH</Link>
       </div>
