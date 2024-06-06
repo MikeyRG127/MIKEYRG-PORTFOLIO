7caa45f41acbf4069c0bf8eb161fed2aea973039
MikeyRG <MikeyRG@DESKTOP-G1MTSAT>
2024-06-05 21:55:03 -0400

    images

--- next.config.js
+++ next.config.js
@@ -2,18 +2,6 @@
 /** @type {import('next').NextConfig} */
 
 
-const nextConfig = {
-    images: {
-        formats: ["image/webp", "image/avif"],
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'ik.imagekit.io',
-                port: '',
-                pathname: '/l9s2o85o5b/profile.png?updatedAt=1717636016869',
-            },
-        ],
-    },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
