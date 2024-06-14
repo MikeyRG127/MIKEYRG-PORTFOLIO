914b8d41e7f8005a1d8183c18d23dd65a5f702e1
MikeyRG <MikeyRG@DESKTOP-G1MTSAT>
2024-06-13 22:12:58 -0400

    more changes

--- components/WorkModal.jsx
+++ components/WorkModal.jsx
@@ -1,10 +1,27 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "@styles/workmodal.css";
 import Image from "next/image";
 
 export default function WorkModal({ title = "default", image = "default", description = "none", src = "none", alt = "none" }) {
     const [modalToggle, setModalToggle] = useState(false);
+
+    useEffect(() => {
+        const handleOutsideClick = (event) => {
+            // Check if the click occurred outside the modal
+            if (modalToggle && !event.target.closest('.work-modal')) {
+                setModalToggle(false); // Close the modal
+            }
+        };
+
+        // Add event listener when component mounts
+        document.addEventListener('click', handleOutsideClick);
+
+        // Remove event listener when component unmounts
+        return () => {
+            document.removeEventListener('click', handleOutsideClick);
+        };
+    }, [modalToggle]); // Re-run effect when modalToggle changes
 
     function activate() {
         setModalToggle(prevState => !prevState);
@@ -23,9 +40,22 @@ export default function WorkModal({ title = "default", image = "default", descri
             />
             <div className={`work-modal ${modalToggle && "active"}`}>
                 <div className="modal-content">
-                    <div onClick={activate}>CLICK HERE TO CLOSE</div>
+                    <div className="topbar">
+                        <Image
+                            onClick={activate}
+                            src={"/assets/icons/cerrar.png"}
+                            alt={"Close Icon"}
+                            width={25}
+                            height={25}
+                            className="modal-close-icon"
+                            loading="lazy">
+                        </Image>
+                    </div>
+
+
                 </div>
             </div>
         </>
     );
 }
+
